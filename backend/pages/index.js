4e629b76aa89900a316c57b34ca0751efee91cba
PabloMacOS <pablogaxiola35@gmail.com>
2025-03-21 18:08:07 +0800

    Index.js implemented

---
+++ backend/pages/index.js
@@ -0,0 +1,18 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          fontFamily: "monospace",
+          backgroundColor: "#f1a252",
+        }}
+      >
+        <h1>Server en línea</h1>
+      </div>
+    );
+  }
+  
